09525d080a96b4b7b0eb19aade83023875cce0ee
David Chisnall <theraven@theravensnest.org>
2017-03-20 10:56:18 +0000

    Add support for running tests with and without -@

--- Tests/testexpected.sh
+++ Tests/testexpected.sh
@@ -3,4 +3,11 @@
 # Simple test: do we get the same output from dts -> dts conversion that we
 # expected?
 
-"$1" -I dts -O dts "$2" | diff "${2}.expected" -
+# For tests named .at.dts, add -@ to the dtc invocation
+AT=
+BASE=`basename "$2"`
+if [ `basename -s .at.dts "$BASE"` != "$BASE" ] ; then
+	AT=-@
+fi
+
+"$1" $AT -I dts -O dts "$2" | diff "${2}.expected" -
--- Tests/testviabin.sh
+++ Tests/testviabin.sh
@@ -3,8 +3,15 @@
 # Test whether dts -> dts conversion gives the same output as dts -> dtb -> dts
 # conversion
 
-"$1" -I dts -O dts "$2" > "${2}.fromdts"
-"$1" -I dts -O dtb "$2" | "${1}" -I dtb -O dts > "${2}.fromdtb"
+# For tests named .at.dts, add -@ to the dtc invocation
+AT=
+BASE=`basename "$2"`
+if [ `basename -s .at.dts "$BASE"` != "$BASE" ] ; then
+	AT=-@
+fi
+
+"$1" $AT -I dts -O dts "$2" > "${2}.fromdts"
+"$1" $AT -I dts -O dtb "$2" | "${1}" -I dtb -O dts > "${2}.fromdtb"
 diff "${2}.fromdts" "${2}.fromdtb"
 DIFF=$?
 rm "${2}.fromdts" "${2}.fromdtb"
