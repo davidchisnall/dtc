2a78bd75069698395ce7d0808eae57b6f911e1a2
Kyle Evans <kevans91@ksu.edu>
2018-02-03 10:56:49 -0600

    Remove ultimately unused local

--- fdt.cc
+++ fdt.cc
@@ -1455,7 +1455,6 @@ device_tree::resolve_cross_references(uint32_t &phandle)
 void
 device_tree::garbage_collect_marked_nodes(node_ptr &n, node *parent)
 {
-	bool deleted = false;
 	if (n->delete_if_unreferenced)
 	{
 		auto i = std::find(referenced_nodes.begin(), referenced_nodes.end(),
