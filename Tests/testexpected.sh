074a7de2e1affdef0513085e7caafa1db8a207ef
David Chisnall <David.Chisnall@cl.cam.ac.uk>
2019-02-23 14:42:27 +0000

    Remove write to /tmp.
    This was a bit ugly as a mechanism for testing binary files and it
    causes some interesting race conditions in CI.

--- Tests/testexpected.sh
+++ Tests/testexpected.sh
@@ -12,18 +12,13 @@ fi
 
 TESTDIR=`dirname "$2"`
 DOCOPY_TEST="incbin_absolute.dts"
-DOCOPY_FROM=${TESTDIR}/bytes
-DOCOPY_TO=/tmp/bsdtc_test_bytes
-
 if [ `basename ${2}` == "${DOCOPY_TEST}" ] ; then
-	cp ${DOCOPY_FROM} ${DOCOPY_TO}
-fi
-
-"$1" $AT -I dts -O dts "$2" | diff "${2}.expected" -
-_exit=$?
-
-if [ `basename ${2}` == "${DOCOPY_TEST}" ] ; then
-	rm ${DOCOPY_TO}
+	sed "s|TESTDIR|${TESTDIR}|g" ${2} | "$1" $AT -I dts -O dts - | diff "${2}.expected" -
+	_exit=$?
+else
+	echo FAIL!
+	"$1" $AT -I dts -O dts "$2" | diff "${2}.expected" -
+	_exit=$?
 fi
 
 exit $_exit
